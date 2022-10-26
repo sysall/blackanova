bb56d26565056291628febbc9f4dc15eaa5f1bff
Birame Sene <birametgod95@gmail.com>
2022-10-26 20:46:25 +0200

    Initialization with old changes

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:blackanova/screens/welcome_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'constants.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+          scaffoldBackgroundColor: kBackgroundColor,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: WelcomePage()
+    );
+  }
+}
